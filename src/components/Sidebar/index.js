c17971d918a546f8448d46e977a4fe2b051dfb97
GUILHERME ADAM RODRIGUES DE OLIVEIRA SIL - 215433 - GETIC <215433@int.cagece.com.br>
2023-08-30 16:16:50 -0300

    Removendo Console

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -71,8 +71,6 @@ export default function Sidebar() {
 
                   const isSelected = selectedItem === name;
 
-                  console.log({name: name, isSelected: isSelected, selectedItem: selectedItem});
-
                   return (
                     <Link key={name} to={path} onClick={() => handleSelectedItem(name)}>
                       <ItemSession isSelected={isSelected}>
